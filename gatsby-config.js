491532f86de50d314e988f079aaf5de1e550b290
“itsalexmejia” <AlexWim1@gmail.com>
2023-05-30 19:02:20 -0600

    First Commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `am-documentation`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
